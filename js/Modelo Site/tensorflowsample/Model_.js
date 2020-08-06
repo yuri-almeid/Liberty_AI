025e742111acc64cc640b935abd26b63f4e94e2e
Yuri Almeida <yurilima95@gmail.com>
2020-08-05 23:51:07 -0300

    Model_ update

--- js/Modelo Site/tensorflowsample/Model_.js
+++ js/Modelo Site/tensorflowsample/Model_.js
@@ -126,16 +126,19 @@ function dataPreprocessing(data_raw){
   console.log("Preprocessamento dos dados concluido.");
   
   // Chama funcao principal
-  aiModel(X, Y);
+  const model = await aiModel(X, Y);
+
+  console.log('Modelo treinado com sucesso');
+
 }
 
 
-function aiModel(X, Y){
+async function aiModel(X, Y){
   console.log('--------- CRIACAO DO MODELO ---------');
 
   const model = tf.sequential();
-  const xs = tf.tensor2d(X, [X.length, X[0].length]).div(tf.scalar(10));
-  const ys = tf.tensor2d(Y, [Y.length, 1]).reshape([Y.length, 1]).div(tf.scalar(10));
+  const x = tf.tensor2d(X, [X.length, X[0].length]).div(tf.scalar(10));
+  const y = tf.tensor2d(Y, [Y.length, 1]).reshape([Y.length, 1]).div(tf.scalar(10));
   
   model.add(tf.layers.dense({units: input_layer_neurons, inputShape: [input_layer_shape]}));
   model.add(tf.layers.reshape({targetShape: rnn_input_shape}));
@@ -165,64 +168,22 @@ function aiModel(X, Y){
     loss: 'meanSquaredError'
   });
 
-
-  
-}
-
-
-async function trainModel(X, Y, window_size, n_epochs, learning_rate, n_layers, callback){
-
-  const input_layer_shape  = window_size;
-  const input_layer_neurons = 100;
-  
-  const rnn_input_layer_features = 10;
-  const rnn_input_layer_timesteps = input_layer_neurons / rnn_input_layer_features;
-  
-  const rnn_input_shape  = [rnn_input_layer_features, rnn_input_layer_timesteps];
-  const rnn_output_neurons = 20;
-  
-  const rnn_batch_size = window_size;
-  
-  const output_layer_shape = rnn_output_neurons;
-  const output_layer_neurons = 1;
-  
-  const model = tf.sequential();
-  
-  const x = tf.tensor2d(X, [X.length, X[0].length]).div(tf.scalar(10));
-  const y = tf.tensor2d(Y, [Y.length, 1]).reshape([Y.length, 1]).div(tf.scalar(10));
-  
-  model.add(tf.layers.dense({units: input_layer_neurons, inputShape: [input_layer_shape]}));
-  model.add(tf.layers.reshape({targetShape: rnn_input_shape}));
-  
-  let lstm_cells = [];
-  for (let index = 0; index < n_layers; index++) {
-        lstm_cells.push(tf.layers.lstmCell({units: rnn_output_neurons}));
-  }
-  
-  model.add(tf.layers.rnn({
-    cell: lstm_cells,
-    inputShape: rnn_input_shape,
-    returnSequences: false
-  }));
-  
-  model.add(tf.layers.dense({units: output_layer_neurons, inputShape: [output_layer_shape]}));
-  
-  model.compile({
-    optimizer: tf.train.adam(learning_rate),
-    loss: 'meanSquaredError'
-  });
-  
   const hist = await model.fit(x, y,
     { batchSize: rnn_batch_size, 
       epochs: n_epochs, 
       callbacks: tfvis.show.fitCallbacks(
-        { name: 'Performace de Treinamento' },
+        { name: 'Performance de Treinamento' },
         ['loss'], 
-        { height: 200, callbacks: ['onEpochEnd'] }
+        { height: 200, 
+          callbacks: ['onEpochEnd'] 
+        }
       )
   });
 
+  return { model: model, stats: hist };  
+  
 }
+
 
 getHistory('frxAUDCAD', 300, 'candles');
 
